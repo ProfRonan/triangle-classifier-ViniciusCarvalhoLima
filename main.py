1c5f316efaa6975b29d379933703adb6692b28d5
ViniciusCarvalhoLima <128156331+ViniciusCarvalhoLima@users.noreply.github.com>
2023-04-21 15:14:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-a = int(input("digite o valor do lado:\n"))
-b = int(input("digite o valor do lado:\n"))
-c = int(input("digite o valor do lado:\n"))
-if a == b and b == c:
-    print("Equilátero")
-if a == b and b != c:
-    print("Isósceles")
-elif a == c and c != b:
-    print("Isósceles")
-elif b == c and c != a:
-    print("Isósceles")
-elif b != c and c != a:
-    print("Escaleno")
+a = int(input("Digite o valor do lado: "))
+b = int(input("Digite o valor do lado: "))
+c = int(input("Digite o valor do lado: "))
+
+triangulo = True
+
+if a + b <= c or a + c <= b or b + c <= a:
+    print("Não é um triângulo")
+    triangulo = False
+
+if triangulo:
+    if a == b and b == c:
+        print("Equilátero")
+    elif a == b or b == c or a == c:
+        print("Isósceles")
+    else:
+        print("Escaleno")
